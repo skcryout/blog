1987659a994cc148a75f2c22e3fe3de57a101f33
Sunghwan Kim <skcryout@gmail.com>
2014-03-03 14:10:56 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VingleBlog::Application.config.secret_key_base = 'ed8f3b0e6deb515b1a27fc7791a7a8c1acac4b20fa4bd775471bf4934bea3a458c69febd45671e53cbf5132355f36a0635ad1effb4882f94399ad67d3885ae87'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VingleBlog::Application.config.session_store :cookie_store, key: '_vingle-blog_session'
