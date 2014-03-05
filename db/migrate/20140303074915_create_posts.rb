865c5ed4083dd94bee69750d6fe6eb151294302f
Sunghwan Kim <skcryout@gmail.com>
2014-03-05 17:14:40 +0900

    DB Association과 꼭 필요할 것 같은 column들  대충 만들어 봄

--- db/migrate/20140303074915_create_posts.rb
+++ db/migrate/20140303074915_create_posts.rb
@@ -1,6 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.integer :user_id
 
       t.timestamps
     end
--- db/migrate/20140303074927_create_tags.rb
+++ db/migrate/20140303074927_create_tags.rb
@@ -1,8 +1,14 @@
 class CreateTags < ActiveRecord::Migration
   def change
     create_table :tags do |t|
+      t.string :tag
 
       t.timestamps
+    end
+
+    create_table :posts_tags, id: false do |t|
+      t.integer :post_id
+      t.integer :tag_id
     end
   end
 end
