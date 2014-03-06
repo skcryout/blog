ce8f0f559161a2b235f9c34dc004690aaf132672
Sunghwan Kim <skcryout@gmail.com>
2014-03-06 09:36:19 +0900

    로그인 과정에서 정에서 토큰이 새로 만들어지지 않던 현상 수정

--- app/services/session.rb
+++ app/services/session.rb
@@ -6,17 +6,18 @@ class Session
 
   def create
     params = create_params
-    exception = exception_handler params
+    exception = exception_handler_for_create params
     return exception if exception
 
 
     user = User.find_by(username: params[:username])
 
     if BCrypt::Password.new(user[:encrypted_password]) == params[:password]
-
+      auth_token = create_authentication_token
+      user.update_attributes(authentication_token: auth_token)
       return {
         statusCode: 100,
-        auth_token: create_authentication_token
+        auth_token: auth_token
       }
     else
       return {
@@ -30,6 +31,10 @@ class Session
         user: @params
     })
     params = params.require(:user).permit(:auth_token)
+
+    exception = exception_handler_for_destroy params
+    return exception if exception
+
     user = User.find_by(authentication_token: params[:auth_token])
     if user
       user.update_attributes(authentication_token: nil)
@@ -54,12 +59,15 @@ class Session
     end
   end
 
-  def exception_handler(params)
+  def exception_handler_for_create(params)
     return {statusCode: -101} if params[:username].length == 0
     return {statusCode: -102} if params[:password].length == 0
     return {statusCode: -103} if !check_username_existance(params[:username])
   end
 
+  def exception_handler_for_destroy(params)
+    return {statusCode: -101} if params[:auth_token].length == 0
+  end
 
   private
     def create_authentication_token
