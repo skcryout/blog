3b6e7d7d316ea3756cfe6fe89f3689fa2db181f1
Sunghwan Kim <skcryout@gmail.com>
2014-03-10 16:42:50 +0900

    nothing

--- app/assets/javascripts/header/TitleController.js
+++ app/assets/javascripts/header/TitleController.js
@@ -1,4 +1,6 @@
 theBlog.controller('TitleController', function($scope, $http) {
+    $scope.users = 0;
+    $scope.posts = 0;
     $http.get('/api/v1/title').success(function(data) {
         $scope.users = data.number_of_users;
         $scope.posts = data.number_of_posts;
